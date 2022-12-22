3a77cfdb94691b4f55bde0d7c3a48c61d762f5f1
semnil5202 <semnil5202@gmail.com>
2022-12-23 00:54:25 +0900

    refactor: 요구 사항에 맞게 BridgeGame 활용
    BridgeGame의 요구사항으로 보아 서비스 클래스가 적당하다고 판단하여 서비스 클래스로 활용한다.

---
+++ src/service/BridgeGame.js
@@ -0,0 +1,7 @@
+const Bridge = require('../model/Bridege');
+const BridgeMaker = require('../BridgeMaker');
+const BridgeNumber = require('../BridgeRandomNumberGenerator');
+
+class BridgeGame {}
+
+module.exports = BridgeGame;
