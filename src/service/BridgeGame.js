0fcae466d3e51be5de22dd749d7ffb11525821a1
semnil5202 <semnil5202@gmail.com>
2022-12-23 00:55:05 +0900

    refactor: DI 수정
    randomNumber를 외부에서 주입해줌으로써 의존성을 제거한다.

--- src/service/BridgeGame.js
+++ src/service/BridgeGame.js
@@ -2,6 +2,10 @@ const Bridge = require('../model/Bridege');
 const BridgeMaker = require('../BridgeMaker');
 const BridgeNumber = require('../BridgeRandomNumberGenerator');
 
-class BridgeGame {}
+class BridgeGame {
+  makeBridge(size) {
+    return BridgeMaker.makeBridge(size, BridgeNumber.generate());
+  }
+}
 
 module.exports = BridgeGame;
