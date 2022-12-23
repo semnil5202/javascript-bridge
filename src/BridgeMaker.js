645fb9707af012593271525715ccb650386e1aa1
semnil5202 <semnil5202@gmail.com>
2022-12-23 17:15:05 +0900

    refactor: JS Docs의 설명과 동일한 매개변수 사용

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -10,11 +10,11 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generator) {
+  makeBridge(size, generateRandomNumber) {
     const bridge = [];
     for (let i = 0; i < size; i++) {
-      const randomNumber = generator();
-      const crossable = this.whereCrossable(randomNumber);
+      const answer = generateRandomNumber();
+      const crossable = this.whereCrossable(answer);
       bridge.push(crossable);
     }
     return bridge;
